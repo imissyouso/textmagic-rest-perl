0b3c762f99ffa77dffa1ee057b8b55b524d7f0ab
Andrey Vorobyev <vorobivka@mail.ru>
2019-09-24 13:09:41 +0000

    Fix PHP readme

--- Makefile.pl
+++ Makefile.pl
@@ -2,7 +2,7 @@ use ExtUtils::MakeMaker;
 
 WriteMakefile(
     NAME            => 'Net::Sms::TextMagicClient',
-    VERSION => {{version}},
+    VERSION => ,
     PREREQ_PM  => {
         "URI::Query" => 0,
         "Log::Any" => 0,
